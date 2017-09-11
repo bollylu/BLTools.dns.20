652bbc86e27f43b6cb0a8313edefcf8ff4546c58
Luc Bolly <lbolly@hotmail.com>
2017-09-11 08:47:44 +0200

    Initial commit

---
+++ BLTools Dot Net Std 2.0/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BLTools_Dot_Net_Std_2._0
+{
+    public class Class1
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# BLTools
+Toolbox for .NET projects
+
+This version is based on .NET Standard 2.0
