10531066487980ba6df7835c3d76f3a750186ca5
Luc Bolly <lbolly@hotmail.com>
2017-11-13 17:02:07 +0100

    Fixed errors and  added tests for JsonEncode

--- BLTools Dot Net Std 2.0/Json/Json.cs
+++ BLTools Dot Net Std 2.0/Json/Json.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using BLTools;
 
 namespace BLTools.Json {
   public static class Json {
@@ -14,7 +16,92 @@ namespace BLTools.Json {
 
     public const int DEFAULT_INDENT = 2;
 
+    private const char CHR_SLASH = '/';
+    private const char CHR_BACKSLASH = '\\';
+    private const char CHR_DOUBLE_QUOTE = '\"';
+
     public static string JsonEncode(this string source) {
+
+      if ( string.IsNullOrEmpty(source) ) {
+        return "";
+      }
+
+      StringBuilder RetVal = new StringBuilder();
+
+      int i = 0;
+      int LengthOfSource = source.Length;
+      bool InQuote = false;
+      bool IsEscapeChar = false;
+
+      do {
+
+        char CurrentChar = source[i];
+        Trace.WriteLine($"Found [{CurrentChar.ToString().ToByteArray().ToHexString()}],[{CurrentChar}]");
+
+
+        if ( CurrentChar == CHR_DOUBLE_QUOTE && !InQuote ) {
+          InQuote = true;
+          Trace.WriteLine("InQuote=true");
+          RetVal.Append(CHR_DOUBLE_QUOTE);
+          i++;
+          continue;
+        }
+
+        if ( CurrentChar == CHR_BACKSLASH && InQuote && IsEscapeChar) {
+          RetVal.Append(CHR_BACKSLASH);
+          IsEscapeChar = false;
+          Trace.WriteLine("IsEscapeChar=false");
+          i++;
+          continue;
+        }
+
+        if (CurrentChar == CHR_BACKSLASH && InQuote) {
+          IsEscapeChar = true;
+          RetVal.Append(CHR_BACKSLASH);
+          Trace.WriteLine("IsEscapeChar=true");
+          i++;
+          continue;
+        }
+
+        if ( "\t\b\r\n\f/".Contains(CurrentChar) && InQuote) {
+          RetVal = RetVal.Append(CHR_BACKSLASH).Append(CurrentChar);
+          Trace.WriteLine($"Adding control char {CurrentChar.ToString()}");
+          i++;
+          continue;
+        }
+        if ( "tbrnf/".Contains(CurrentChar) && IsEscapeChar ) {
+          RetVal = RetVal.Append(CurrentChar);
+          Trace.WriteLine($"Adding control char {CurrentChar.ToString()}");
+          i++;
+          continue;
+        }
+
+        if ( CurrentChar == CHR_DOUBLE_QUOTE && InQuote && IsEscapeChar) {
+          RetVal.Append(CHR_DOUBLE_QUOTE);
+          Trace.WriteLine("Adding double quote inside quote");
+          i++;
+          continue;
+        }
+
+        if ( CurrentChar == CHR_DOUBLE_QUOTE && InQuote) {
+          InQuote = false;
+          Trace.WriteLine("InQuote=false");
+          RetVal.Append(CHR_DOUBLE_QUOTE);
+          i++;
+          continue;
+        }
+
+        RetVal = RetVal.Append(CurrentChar);
+        i++;
+
+        Trace.WriteLine($"{RetVal.ToString()}");
+
+      } while ( i < LengthOfSource );
+
+      return RetVal.ToString();
+    } 
+
+    public static string JsonDecode(this string source) {
 
       if ( string.IsNullOrEmpty(source) ) {
         return "";
@@ -31,7 +118,7 @@ namespace BLTools.Json {
 
         char CurrentChar = source[i];
 
-        if (InQuote) {
+        if ( InQuote ) {
 
           if ( !NextCharIsControlChar && CurrentChar == '\\' ) {
             NextCharIsControlChar = true;
@@ -39,20 +126,14 @@ namespace BLTools.Json {
             continue;
           }
 
-          if ( NextCharIsControlChar && "\"\\\t\b\r\n\f".Contains(CurrentChar) ) {
+          if ( NextCharIsControlChar && "\"\\\t\b\r\n\f/".Contains(CurrentChar) ) {
             NextCharIsControlChar = false;
-            RetVal.Append('\\');
             RetVal.Append(CurrentChar);
             i++;
             continue;
           }
 
-          if ( CurrentChar == '/' ) {
-            RetVal.Append('\\');
-            RetVal.Append(CurrentChar);
-            i++;
-            continue;
-          }
+
 
           if ( CurrentChar == '"' ) {
             RetVal.Append(CurrentChar);
---
+++ BLTools Tests Core 2.0/Json/JsonEncodeTest.cs
@@ -0,0 +1,96 @@
+﻿using BLTools;
+using BLTools.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BLTools.UnitTest.Core20.Json {
+
+
+  [TestClass()]
+  public class JsonEncodeTest {
+
+    #region --- Tests support --------------------------------------------
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext {
+      get {
+        return testContextInstance;
+      }
+      set {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    // 
+    //You can use the following additional attributes as you write your tests:
+    //
+    //Use ClassInitialize to run code before running the first test in the class
+    //[ClassInitialize()]
+    //public static void MyClassInitialize(TestContext testContext)
+    //{
+    //}
+    //
+    //Use ClassCleanup to run code after all tests in a class have run
+    //[ClassCleanup()]
+    //public static void MyClassCleanup()
+    //{
+    //}
+    //
+    //Use TestInitialize to run code before running each test
+    //[TestInitialize()]
+    //public void MyTestInitialize()
+    //{
+    //}
+    //
+    //Use TestCleanup to run code after each test has run
+    //[TestCleanup()]
+    //public void MyTestCleanup()
+    //{
+    //}
+    //
+    #endregion 
+    #endregion --- Tests support --------------------------------------------
+
+
+    [TestMethod(), TestCategory("NC20.Json.JsonEncode")]
+    public void JsonEncode_EmptyString_ResultEmpty() {
+      string Actual = "".JsonEncode();
+      Assert.AreEqual("", Actual);
+    }
+
+    [TestMethod(), TestCategory("NC20.Json.JsonEncode")]
+    public void JsonEncode_SimpleString_ResultIsTheSame() {
+      string Actual = "this is a test".JsonEncode();
+      Assert.AreEqual("this is a test", Actual);
+    }
+
+    [TestMethod(), TestCategory("NC20.Json.JsonEncode")]
+    public void JsonEncode_StringWithControlChars_ResultIsEncoded() {
+      string Actual = @"""this\tis\fan\\encoding/test""".JsonEncode();
+      Assert.AreEqual(@"""this\tis\fan\\encoding\/test""", Actual);
+    }
+
+    [TestMethod(), TestCategory("NC20.Json.JsonEncode")]
+    public void JsonEncode_StringWithQuotes_ResultIsEncoded() {
+      string Actual = "this \"is a\" test".JsonEncode();
+      Assert.AreEqual("this \"is a\" test", Actual);
+    }
+
+    [TestMethod(), TestCategory("NC20.Json.JsonEncode")]
+    public void JsonEncode_StringWithQuotesAndControlChars_ResultIsEncoded() {
+      string Actual = @"""this ""is\\a"" test""".JsonEncode();
+      Assert.AreEqual(@"""this ""is\\a"" test""", Actual);
+    }
+
+    [TestMethod(), TestCategory("NC20.Json.JsonEncode")]
+    public void JsonEncode_ObjectWithQuotesAndControlChars_ResultIsEncoded() {
+      string Actual = @"{""t"":""{\\""a\\"":1,\\""b\\"":\\""a sample text\\""}"",""c"":2,""r"":""some text""}".JsonEncode();
+      Assert.AreEqual(@"{""t"":""{\\""a\\"":1,\\""b\\"":\\""a sample text\\""}"",""c"":2,""r"":""some text""}", Actual);
+    }
+  }
+}
